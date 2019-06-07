f6676c2dde4d90c8a15c8cccc99ad807d71a9304
Ewerton Silva Santos <ewertonsilva@mucuri.grad.dcc.ufmg.br>
2019-06-07 17:40:45 -0300

    merge

--- tests/teste_heroi.cpp
+++ tests/teste_heroi.cpp
@@ -4,63 +4,30 @@
 
 //using namespace  std;
 
-#define VIDA 100
-#define ATAQUE 10
-#define DEFESA 80
-#define VIVO true
-
-TEST_CASE("1 - [Heroi] - Construtor/Getters ") {
-    /*Heroi heroi = Heroi(100, 10, 80,"HEROI1");
-
-    CHECK_EQ(heroi.get_pt_vida(), VIDA);
-    CHECK_EQ(heroi.get_pt_ataque(), ATAQUE);
-    CHECK_EQ(heroi.get_pt_defesa(), DEFESA);*/
-    std::cout << "Chegaaaa" ;
-}
-/*
-TEST_CASE("2 - [Heroi] - Construtor/THROWS") {
-    CHECK_THROWS(Heroi(-100, 10, 80));
-    CHECK_THROWS(Heroi(100, -10, 80));
-    CHECK_THROWS(Heroi(100, 10, -80));
+TEST_CASE("1 - [Heroi] - Construtor") {
+    //Heroi heroi = Heroi(100, 10, 80);
+    //CHECK(heroi.get_pt_vida, 100);
+    //CHECK(heroi.get_pt_ataque, 11);
+    //CHECK(heroi.get_pt_defesa, 80);
 }
 
-TEST_CASE("3 - [Heroi] - Diminui vida") {
-	Heroi heroi = Heroi(100, 10, 80);
-	heroi.diminui_vida(10);
+//TEST_CASE("2 - [Heroi] - Getters") {
+//}
+//
+//TEST_CASE("3 - [Heroi] - Diminui vida") {
+//}
+//
+//TEST_CASE("4 - [Heroi] - Realiza ataque") {
+//}
 
-    CHECK_EQ(heroi.get_pt_vida(), VIDA-10);
-}
+//Heroi::Heroi(int pt_vida, int pt_ataque, int defesa);
+    
+//void Heroi::diminui_vida(const int dano_recebido);
 
-TEST_CASE("4 - [Heroi] - Diminui vida/Negativo") {
-	Heroi heroi = Heroi(100, 10, 80);
-	heroi.diminui_vida(-10);
+//const int Heroi::realiza_ataque();
 
-    CHECK_THROWS(heroi.get_pt_vida(), VIDA-10);
-}
+//int Heroi::get_ataque_com_bonus();
 
-TEST_CASE("5 - [Heroi] - Realiza ataque") {
-	Heroi heroi = Heroi(100, 10, 80);
+//bool Heroi::get_vivo();
 
-    CHECK_THROWS(heroi.realiza_ataque(), ATAQUE);
-}
 
-TEST_CASE("6 - [Heroi] - Ataque com bonus") {
-	Heroi heroi = Heroi(100, 10, 80);
-
-    CHECK_EQ(heroi.realiza_ataque(), ceil(ATAQUE*0.5));
-}
-
-TEST_CASE("7 - [Heroi] - Heroi vivo") {
-	Heroi heroi = Heroi(100, 10, 80);
-
-    CHECK_EQ(heroi.get_vivo(), VIVO);
-}
-
-TEST_CASE("8 - [Heroi] - Heroi morto") {
-	Heroi heroi = Heroi(100, 10, 80);
-	heroi.diminui_vida(heroi.get_vivo);
-
-    CHECK_EQ(heroi.get_vivo(),!VIVO);
-}
-
-*/
