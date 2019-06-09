e19552a9aeda99df71b2309e6736a0e2501936ce
Julia <42300403+julinha13@users.noreply.github.com>
2019-06-09 10:55:52 -0300

    Update partida.cpp
    Tratamento de exceções utilizando Try-Cath na classe partida.

--- src/partida.cpp
+++ src/partida.cpp
@@ -4,16 +4,20 @@ void Partida::set_dificuldade(){
     system("clear");
     char opcao;
     //tela_dificuldade();
-    do{
+    try{
         //todo - colocar a tela dificuldade quando estiver pronta
         std::cout << "Aqui vem o texto da tela dificuldade. Digite F, M ou D"<< std::endl;
         std::cin >> opcao;
-        std::cout << "você digitou: " << opcao << std::endl;
         opcao = tolower(opcao);
         std::cout << "você digitou: " << opcao << std::endl;
-        if (opcao != 'f' && opcao != 'm' && opcao != 'd')
-            std::cout << "Por favor, digite uma opção válida" << std::endl;
-    }while(opcao != 'f' && opcao != 'm' && opcao != 'd');
+        
+        if (opcao != 'f' && opcao != 'm' && opcao != 'd')  
+            throw std::invalid_argument("Opção Inválida");
+        
+    }catch(const std::invalid_argument& e){
+        std::cout << "Por favor, digite uma opção válida" << std::endl; //Não ta aparecendo esse cout por causa do system("clear")
+        Partida::set_dificuldade();
+    }
 
     this->dificuldade = opcao;
 }
