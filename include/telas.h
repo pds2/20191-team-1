5618fad82473257e489af90ac0205cf64cccc7a7
Valerieps <souza.pereira.valeria@gmail.com>
2019-06-10 01:15:42 +0000

    tela morte heroi sem pedra finalizada

--- include/telas.h
+++ include/telas.h
@@ -16,11 +16,14 @@ void mostra_equipe(std::set<Heroi> equipe);
 void tela_vitoria_jogador();
 void tela_vitoria_thanos();
 void tela_resultado_ataque_thanos(std::string heroi, int dano_causado, std::string nome_ataque);
+void tela_morte_heroi_sem_pedra(std::string nome_heroi);
+
+
 
 //    void tela_personagem();
 //    void tela_resultado_ataque_jogador();
 //    void tela_descricao_persoangem(int id);
-//    void tela_morte_heroi_sem_pedra();
+
 //    void tela_batalha();
 //    void tela_vitoria_thanos();
 
