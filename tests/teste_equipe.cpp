c86083a25331cc4832daea52912f6bc442577a9e
EwertonDCSilv <ewertonsilva@dcc.ufmg.br>
2019-06-12 20:52:55 -0300

    Comentando teste - Equipe

--- tests/teste_equipe.cpp
+++ tests/teste_equipe.cpp
@@ -14,7 +14,9 @@
 #define TMAX_ID 10
 #define TNUM_EQ 5
 
-
+/* 
+*   @method {Para testar o construtor e gets da classes Equipe}
+*/
 TEST_CASE("1 - [Equipe] - Construtor/Getters ") {
     Equipe equipe = Equipe();
 
@@ -23,6 +25,9 @@ TEST_CASE("1 - [Equipe] - Construtor/Getters ") {
     CHECK_EQ(equipe.get_id_portador_pedra(), ID_HEROI);
 }
 
+/* 
+*   @method {Para testar se um Heroi e adicionado a Equipe}
+*/
 TEST_CASE("2 - [Equipe] - Adiciona Equipe") {
 	Equipe equipe = Equipe();
 	equipe.adiciona_heroi(ADD_HEROI1);
@@ -30,6 +35,9 @@ TEST_CASE("2 - [Equipe] - Adiciona Equipe") {
     CHECK_EQ(equipe.get_herois_selecionados(), NUM_HEROIS_SELEC+1);
 }
 
+/* 
+*   @method {Para testar erro ao inserir Heroi repitido}
+*/
 TEST_CASE("3 - [Equipe] - Adiciona Heroi/Repitido") {
     Equipe equipe = Equipe();
     
@@ -37,12 +45,18 @@ TEST_CASE("3 - [Equipe] - Adiciona Heroi/Repitido") {
     CHECK_THROWS(equipe.adiciona_heroi(0));
 }
 
+/* 
+*   @method {Para testar erro ao inserir id fora do intervalo}
+*/
 TEST_CASE("4 - [Equipe] - Adiciona Heroi/Id negativo") {
     Equipe equipe = Equipe();
 
     CHECK_THROWS(equipe.adiciona_heroi(ADD_HEROI2));
 }
 
+/* 
+*   @method {Para testar a remocao de Herois da Equipe}
+*/
 TEST_CASE("5 - [Equipe] - Remove Heroi") {
     Equipe equipe = Equipe();
     int i = 0;
@@ -53,18 +67,27 @@ TEST_CASE("5 - [Equipe] - Remove Heroi") {
     CHECK_EQ(equipe.get_num_herois_vivos() , i-1);
 }
 
+/* 
+*   @method {Para testar erros ao remover Heroi fora da Equipe}
+*/
 TEST_CASE("6 - [Equipe] - Remove Heroi/THROWS") {
     Equipe equipe = Equipe();
     
     CHECK_THROWS(equipe.remove_heroi(ADD_HEROI1));
 }
 
+/* 
+*   @method {Para testar erros ao remover Heroi fora da Equipe com id invalido}
+*/
 TEST_CASE("7 - [Equipe] - Remove Heroi/THROWS") {
     Equipe equipe = Equipe();
     
     CHECK_THROWS(equipe.remove_heroi(ADD_HEROI2));
 }
 
+/* 
+*   @method {Para testar o metodo que define quem e o portador da pedra}
+*/
 TEST_CASE("8 - [Equipe] - Set portador pedra") {
     Equipe equipe = Equipe();
     equipe.adiciona_heroi(1);
@@ -73,11 +96,21 @@ TEST_CASE("8 - [Equipe] - Set portador pedra") {
     CHECK_EQ(equipe.get_id_portador_pedra() , ADD_HEROI1);
 }
 
+/* 
+*   @method {
+*        Para testar o erro no metodo que define quem e o 
+*        portador da pedra quando Equipe esta vazia
+*    }
+*/
 TEST_CASE("9 - [Equipe] - Set portador pedra/THROWS/Vazio") {
     Equipe equipe = Equipe();
     
     CHECK_THROWS(equipe.set_portador_pedra(ADD_HEROI2));
 }
+
+/* 
+*   @method {Para testar erro do metodo que define quem e o portador da pedra com id invalido}
+*/
 TEST_CASE("10 - [Equipe] - Set portador pedra/THROWS/Invalido") {
     Equipe equipe = Equipe();
     equipe.adiciona_heroi(1);
@@ -85,6 +118,9 @@ TEST_CASE("10 - [Equipe] - Set portador pedra/THROWS/Invalido") {
     CHECK_THROWS(equipe.set_portador_pedra(TMP_MAX+1));
 }
 
+/* 
+*   @method {Para testar o metodo que verifica se o portado da pedra esta vivo}
+*/
 TEST_CASE("11 - [Equipe] - Portador Vivo") {
     Equipe equipe = Equipe();
     
@@ -94,36 +130,55 @@ TEST_CASE("11 - [Equipe] - Portador Vivo") {
     CHECK_EQ(equipe.portador_pedra_esta_vivo(),!VIVO);
 }
 
+/* 
+*   @method {Para testar o metodo que monta a Equipe}
+*/
 TEST_CASE("12 - [Equipe] - Monta Equipe") {
     Equipe equipe = Equipe();
 
     CHECK_EQ(equipe.portador_pedra_esta_vivo(),!VIVO);
 }
 
+/* 
+*   @method {Para testar excepeions do metodo que define quem e o portador da pedra}
+*/
 TEST_CASE("12 - [Equipe] - Mosta Equipe") {
     Equipe equipe = Equipe();
     
     CHECK_EQ(equipe.portador_pedra_esta_vivo(),!VIVO);
 }
 
+/* 
+*   @method {Para testar o metodo que realiza danos sobre a Equipe}
+*/
 TEST_CASE("12 - [Equipe] - Equipe sofre Dano") {
     Equipe equipe = Equipe();
     
     CHECK_EQ(equipe.portador_pedra_esta_vivo(),!VIVO);
 }
 
+
+/* 
+*   @method {Para testar o metodo que define qual Heroi ataca}
+*/
 TEST_CASE("12 - [Equipe] - Realiza Ataque") {
     Equipe equipe = Equipe();
     
     CHECK_EQ(equipe.portador_pedra_esta_vivo(),!VIVO);
 }
 
+/* 
+*   @method {Para testar o metodo que verifica o status de vida do Heroi}
+*/
 TEST_CASE("12 - [Equipe] - Status Heroi") {
     Equipe equipe = Equipe();
     
     CHECK_EQ(equipe.portador_pedra_esta_vivo(),!VIVO);
 }
 
+/* 
+*   @method {Para testar o metodo que verifica se a equipe esta total/completa}
+*/
 TEST_CASE("12 - [Equipe] - Equipe Completa/Total") {
     
     Equipe equipe = Equipe();
@@ -134,6 +189,9 @@ TEST_CASE("12 - [Equipe] - Equipe Completa/Total") {
     CHECK_EQ(equipe.get_equipe_completa() , !TIME_COMPLETO);
 }
 
+/* 
+*   @method {Para testar o metodo que verifica se a equipe esta parcila/completa}
+*/
 TEST_CASE("12 - [Equipe] - Equipe Completa/ Parcial") {
     Equipe equipe = Equipe();
     int i = 1;
